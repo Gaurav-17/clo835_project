ff2ac600e1999f82fbc9fadd163f59c093028ba8
138956222-myseneca <eggonzalez-cardiel@myseneca.ca>
2024-03-28 23:22:20 -0400

    Remove colors code, renaming of variables and add image bg in all html pages

--- docker_images/app/app.py
+++ docker_images/app/app.py
@@ -1,19 +1,16 @@
 from flask import Flask, render_template, request
 from pymysql import connections
 import os
-import random
-import argparse
 
 
 app = Flask(__name__)
-BUCKET = os.environ.get("s3link")
 
 DBHOST = os.environ.get("DBHOST") or "localhost"
 DBUSER = os.environ.get("DBUSER") or "root"
 DBPWD = os.environ.get("DBPWD") or "passwors"
 DATABASE = os.environ.get("DATABASE") or "employees"
-COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
-DBPORT = int(os.environ.get("DBPORT"))
+DBPORT = int(os.environ.get("DBPORT")) or 3306
+S3_IMAGE_URI = os.environ.get("S3_IMAGE_URI")
 # Create a connection to the MySQL database
 db_conn = connections.Connection(
     host= DBHOST,
@@ -21,39 +18,18 @@ db_conn = connections.Connection(
     user= DBUSER,
     password= DBPWD,
     db= DATABASE
-
 )
 output = {}
 table = 'employee';
 
-# Define the supported color codes
-color_codes = {
-    "red": "#e74c3c",
-    "green": "#16a085",
-    "blue": "#89CFF0",
-    "blue2": "#30336b",
-    "pink": "#f4c2c2",
-    "darkblue": "#130f40",
-    "lime": "#C1FF9C",
-}
-
-
-# Create a string of supported colors
-SUPPORTED_COLORS = ",".join(color_codes.keys())
-
-# Generate a random color
-COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
-
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    s3_image = BUCKET
-    return render_template('addemp.html', color=color_codes[COLOR], image=s3_image)
+    return render_template('addemp.html', image=S3_IMAGE_URI)
 
 @app.route("/about", methods=['GET','POST'])
 def about():
-    s3_image = BUCKET
-    return render_template('about.html', color=color_codes[COLOR], image=s3_image)
+    return render_template('about.html', image=S3_IMAGE_URI)
 
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
@@ -62,13 +38,10 @@ def AddEmp():
     last_name = request.form['last_name']
     primary_skill = request.form['primary_skill']
     location = request.form['location']
-
-
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
     try:
-
         cursor.execute(insert_sql,(emp_id, first_name, last_name, primary_skill, location))
         db_conn.commit()
         emp_name = "" + first_name + " " + last_name
@@ -77,11 +50,11 @@ def AddEmp():
         cursor.close()
 
     print("all modification done...")
-    return render_template('addempoutput.html', name=emp_name, color=color_codes[COLOR])
+    return render_template('addempoutput.html', name=emp_name, image=S3_IMAGE_URI)
 
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
-    return render_template("getemp.html", color=color_codes[COLOR])
+    return render_template("getemp.html", image=S3_IMAGE_URI)
 
 
 @app.route("/fetchdata", methods=['GET','POST'])
@@ -95,14 +68,12 @@ def FetchData():
     try:
         cursor.execute(select_sql,(emp_id))
         result = cursor.fetchone()
-
         # Add No Employee found form
         output["emp_id"] = result[0]
         output["first_name"] = result[1]
         output["last_name"] = result[2]
         output["primary_skills"] = result[3]
         output["location"] = result[4]
-
     except Exception as e:
         print(e)
 
@@ -110,29 +81,7 @@ def FetchData():
         cursor.close()
 
     return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
-                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], color=color_codes[COLOR])
+                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], image=S3_IMAGE_URI)
 
 if __name__ == '__main__':
-
-    # Check for Command Line Parameters for color
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False)
-    args = parser.parse_args()
-
-    if args.color:
-        print("Color from command line argument =" + args.color)
-        COLOR = args.color
-        if COLOR_FROM_ENV:
-            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
-    elif COLOR_FROM_ENV:
-        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
-        COLOR = COLOR_FROM_ENV
-    else:
-        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
-
-    # Check if input color is a supported one
-    if COLOR not in color_codes:
-        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
-        exit(1)
-
     app.run(host='0.0.0.0',port=81,debug=True)
